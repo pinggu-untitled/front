93c81b6f2d42a8a6bb9ffb565c5bfd140df5ebfe
rovxxmjxx <rovxxmjxx@gmail.com>
2022-09-26 01:30:01 +0900

    [Modified] AppLayout

--- layouts/FullLayout/index.tsx
+++ layouts/FullLayout/index.tsx
@@ -8,10 +8,24 @@ export interface IProps {
 export const Base = styled.div`
   width: 100vw;
   height: 100vh;
+  //background-color: rgba(0, 0, 0, 0.4);
 `;
 
+export const Header = styled.header`
+  width: 100%;
+  height: 70px;
+  border-bottom: 1px solid #dfdfdf;
+`;
+export const Main = styled.div``;
+export const Footer = styled.footer``;
+
 const FullLayout: FC<IProps> = ({ children }) => {
-  return <Base>{children}</Base>;
+  return (
+    <Base>
+      <Header></Header>
+      {children}
+    </Base>
+  );
 };
 
 export default FullLayout;
