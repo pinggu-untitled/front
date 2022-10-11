41b55d848b4db921a6979f1fa6e157733ba06069
EunhaLim <dmsgk1559@gmail.com>
2022-10-11 14:38:28 +0900

    [none]

--- components/Map/index.tsx
+++ components/Map/index.tsx
@@ -7,8 +7,9 @@ const Map = () => {
     const options = {
       center: new kakao.maps.LatLng(33.450701, 126.570667),
       level: 3
-    };
+    }
     const map = new kakao.maps.Map(container, options);
+    
   }, []);
   return (
     <div id='myMap' style={{
