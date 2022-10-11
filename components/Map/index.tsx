c3480818dafdb636ef3f92e58c7da062dbcfd2ed
EunhaLim <dmsgk1559@gmail.com>
2022-10-12 03:10:38 +0900

    [Modified] Set the current position as the center coordinates

--- components/Map/index.tsx
+++ components/Map/index.tsx
@@ -1,42 +1,24 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useCallback } from 'react';
 const { kakao } = window;
 
-interface ILocation {
-  loaded: boolean
-  coordinates?: { lat: number, lng: number }
-  error?: { code: number, message: string }
-}
-
 const Map = () => {
-  const [ location, setLocation ] = useState<ILocation>({
-    loaded: false,
-    coordinates: { lat: 0, lng: 0 }
-  });
-
-  const onSuccess = useCallback((position: { coords: { latitude: number, longitude: number } }) => {
-    setLocation(prev => ({
-      loaded: true,
-      coordinates: { lat: position.coords.latitude, lng: position.coords.longitude }
-    }));
-  }, [location]);
-
-  const onError = useCallback((error: { code: number, message: string }) => {
-    setLocation(prev => ({
-      loaded: true,
-      error
-    }));
-  }, [location]);
-
   useEffect(() => {
+    navigator?.geolocation.getCurrentPosition(onSuccess, onError);
+  }, []);
+
+  const onSuccess = useCallback(({coords: { latitude, longitude }}) => {
     const container = document.getElementById('myMap');
     const options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      center: new kakao.maps.LatLng(latitude, longitude),
       level: 3
-    }
+    };
     const map = new kakao.maps.Map(container, options);
     map.setZoomable(false);
+  }, []);
 
-    navigator?.geolocation.getCurrentPosition(onSuccess, onError);
+  const onError = useCallback(error => {
+    const container = document.getElementById('myMap');
+    if (container) container.innerText = '지도를 표시할 수 없습니다.';
   }, []);
 
   return (
